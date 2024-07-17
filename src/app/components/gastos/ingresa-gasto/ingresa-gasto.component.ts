cd784699b35ceacf9d925fb5b6d0bab33a87d271
Alejandro Raya <alejandroraya09@hotmail.com>
2024-07-17 01:17:17 -0600

    PresupuestoApp Cascaron

---
+++ src/app/components/gastos/ingresa-gasto/ingresa-gasto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresa-gasto',
+  standalone: true,
+  imports: [],
+  templateUrl: './ingresa-gasto.component.html',
+  styleUrl: './ingresa-gasto.component.css'
+})
+export class IngresaGastoComponent {
+
+}
